ca2e89343234b7e5a049f61d1b179a3119fac696
Keiyakushi <nikulin-7@ya.ru>
2023-01-08 22:08:03 +0300

    second

---
+++ app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.practicum.playlistmaker
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("WrongViewCast")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.search)
+        val button2 = findViewById<Button>(R.id.media)
+        val button3 = findViewById<Button>(R.id.setting)
+        button.setOnClickListener{
+         val searchIntent = Intent(this,SearchActivity::class.java)
+         startActivity(searchIntent)
+        }
+        button2.setOnClickListener{
+            val mediaIntent = Intent(this,MediaActivity::class.java)
+            startActivity(mediaIntent)
+        }
+        button3.setOnClickListener{
+            val settingsIntent = Intent(this,SettingsActivity::class.java)
+            startActivity(settingsIntent)
+        }
+
+    }
+}
