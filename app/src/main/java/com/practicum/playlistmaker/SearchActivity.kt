9dea60d5d518b8e57c6e1bf9a5829aab93fef5e9
Keiyakushi <nikulin-7@ya.ru>
2023-02-05 05:55:40 +0300

    Sprint №10

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -7,10 +7,19 @@ import android.os.Bundle
 import android.os.PersistableBundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.view.LayoutInflater
+import android.view.RoundedCorner
 import android.view.View
+import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.view.menu.MenuView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 
 class SearchActivity : AppCompatActivity() {
     companion object {
@@ -18,7 +27,7 @@ class SearchActivity : AppCompatActivity() {
     }
 
     private lateinit var inputText: EditText
-    private lateinit var savedText : String
+    private lateinit var savedText: String
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
         outState.putString(SEARCH_TEXT, savedText)
@@ -38,6 +47,40 @@ class SearchActivity : AppCompatActivity() {
         inputText = findViewById(R.id.search_edit_text)
         val image = findViewById<ImageView>(R.id.back_icon_search)
         inputText.requestFocus()
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        val trackList = arrayListOf(
+            Track(
+                "Smells Like Teen Spirit",
+                "Nirvana",
+                "5:01",
+                "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"
+            ), Track(
+                "Billie Jean",
+                "Michael Jackson",
+                "4:35",
+                "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg"
+            ), Track(
+                "Stayin' Alive",
+                "Bee Gees",
+                "4:10",
+                "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg"
+            ), Track(
+                "Whole Lotta Love",
+                "Led Zeppelin",
+                "5:33",
+                "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg"
+            ), Track(
+                "Sweet Child O'Mine",
+                "Guns N' Roses",
+                "5:03",
+                "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"
+            )
+        )
+
+        val trackAdapter = TrackAdapter(trackList)
+        recyclerView.adapter = trackAdapter
+
         image.setOnClickListener {
             finish()
         }
@@ -70,5 +113,41 @@ class SearchActivity : AppCompatActivity() {
             }
         }
         inputText.addTextChangedListener(textWatcher)
+    }
+
+    data class Track(
+        val trackName: String, val artistName: String,
+        val trackTime: String, val artworkUrl100: String
+    )
+
+    class TrackViewHolder(parentView: ViewGroup) : RecyclerView.ViewHolder(
+        LayoutInflater.from(parentView.context).inflate(R.layout.track_list_view, parentView, false)
+    ) {
+        private val trackName = itemView.findViewById<TextView>(R.id.track_name)
+        private val artistName: TextView = itemView.findViewById(R.id.artist_name)
+        private val trackTime: TextView = itemView.findViewById(R.id.track_time)
+        private val artwork: ImageView = itemView.findViewById(R.id.artwork)
+        fun bind(model: Track) {
+            trackName.text = model.trackName
+            artistName.text = model.artistName
+            trackTime.text = model.trackTime
+            Glide.with(itemView)
+                .load(model.artworkUrl100)
+                .placeholder(R.drawable.ic_none_image)
+                .centerCrop()
+                .transform(RoundedCorners(itemView.resources.getDimensionPixelSize(R.dimen.corner_dp)))
+                .into(artwork)
+        }
+    }
+
+    class TrackAdapter(private val tracks: List<Track>) : RecyclerView.Adapter<TrackViewHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = TrackViewHolder(parent)
+
+        override fun getItemCount() = tracks.size
+
+        override fun onBindViewHolder(holder: TrackViewHolder, position: Int) {
+            holder.bind(tracks[position])
+        }
     }
 }
