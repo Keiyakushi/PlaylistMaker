612b10565ac1b57127ad8bd44e7562af488bac9c
Keiyakushi <nikulin-7@ya.ru>
2023-03-21 04:57:22 +0300

    Sprint №13

--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -27,7 +27,6 @@ class SettingsActivity : AppCompatActivity() {
             SWITCH_PREFERENCES_KEY,false)
         switchThemeMode.setOnCheckedChangeListener { button, isChecked ->
             (applicationContext as App).switchTheme(isChecked)
-            (applicationContext as App).saveTheme(isChecked)
         }
 
         settingsShare.setOnClickListener {
