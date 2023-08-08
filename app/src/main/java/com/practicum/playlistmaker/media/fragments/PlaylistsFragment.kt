1388e3c09e3839b77aadbdbd3869d1240c7740ae
Keiyakushi <nikulin-7@ya.ru>
2023-08-08 20:17:54 +0300

    Sprint №22

--- app/src/main/java/com/practicum/playlistmaker/media/fragments/PlaylistsFragment.kt
+++ app/src/main/java/com/practicum/playlistmaker/media/fragments/PlaylistsFragment.kt
@@ -5,19 +5,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.practicum.playlistmaker.R
 import com.practicum.playlistmaker.databinding.FragmentPlaylistsBinding
 import com.practicum.playlistmaker.media.view_model.PlaylistsViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class PlaylistsFragment : Fragment() {
     private val viewModel by viewModel<PlaylistsViewModel>()
-
+    private lateinit var binding: FragmentPlaylistsBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        val binding = FragmentPlaylistsBinding.inflate(inflater, container, false)
+        binding = FragmentPlaylistsBinding.inflate(inflater, container, false)
         return binding.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btAddPlaylists.setOnClickListener{
+        findNavController().navigate(R.id.action_mediaFragment_to_createPlaylistsFragment)
+        }
+
     }
 
     companion object {
